c9458f0f34b6c6b490007652e454affcbe182ac7
phillip-hirsch <40609080+phillip-hirsch@users.noreply.github.com>
2024-11-17 16:29:02 -0500

    feat: Add shadcn/ui, remove tailwindcss prettier plugin, format files… (#9)
    * feat: Add shadcn/ui, remove tailwindcss prettier plugin, format files with prettier, add shadcn/ui button and dropdown menu, and set up light and dark mode with a toggle button
    
    * chore: Format tailwind config file

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,16 +1,18 @@
+import { ModeToggle } from '@/components/shared/ModeToggle'
 import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 
 export const Route = createRootRoute({
   component: () => (
     <>
-      <div className="p-2 flex gap-2">
-        <Link to="/" className="[&.active]:font-bold">
+      <div className='p-2 flex gap-2 items-center'>
+        <Link to='/' className='[&.active]:font-bold'>
           Home
         </Link>{' '}
-        <Link to="/about" className="[&.active]:font-bold">
+        <Link to='/about' className='[&.active]:font-bold'>
           About
         </Link>
+        <ModeToggle />
       </div>
       <hr />
       <Outlet />
